53245328e4b4075a4d8e75e6e63a59ace7376ae1
Larson Mwenda <larsonmwendah07@gmail.com>
2025-05-27 00:36:58 +0300

    all

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+// 6-multi_languages_loop.js
+
+const messages = ["C is fun", "Python is cool", "JavaScript is amazing"];
+let output = "";
+
+for (const msg of messages) {
+  output += msg + "\n";
+}
+
+console.log(output.trim());
